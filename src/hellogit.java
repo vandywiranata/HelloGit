5d49431745b87ea1cd42895d112cde439874e980
vandywiranata <vandywiranata@gmail.com>
2018-01-16 19:05:30 +0700

    Initial Commit2

---
+++ src/hellogit.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class hellogit {
+    public static void main (String []args){
+        Scanner input = new Scanner (System.in);
+        System.out.println("Hello Git");
+        
+        String name = input.nextLine();
+        System.out.println("Hello" + name);
+    }
+}
